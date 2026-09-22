80bf48f3ca0c2d91df4ca912fa6c2ff072a052ec
KonyaZsofia <konya.zsofia@student.ms.sapientia.ro>
2026-09-22 11:52:51 +0300

    labor_02 futofeny megvalositasa

---
+++ labor_01/PeldaProjekt/External/Inc/pelda.h
@@ -0,0 +1,13 @@
+/*
+ * pelda.h
+ *
+ *  Created on: Sep 21, 2026
+ *      Author: ZenBook
+ */
+
+#ifndef INC_PELDA_H_
+#define INC_PELDA_H_
+
+int peldaFuggveny();
+
+#endif /* INC_PELDA_H_ */
---
+++ labor_01/PeldaProjekt/External/Src/pelda.c
@@ -0,0 +1,15 @@
+/*
+ * pelda.c
+ *
+ *  Created on: Sep 21, 2026
+ *      Author: ZenBook
+ */
+
+
+#include "pelda.h"
+
+int peldaFuggveny()
+{
+    // Itt csinálunk valamit
+    return 0;
+}
